0fffacada23e2638ecddb5187ea4b614961ea24e
Farid Mahmudov <Razormad666@gmail.com>
2024-02-19 22:22:43 +0300

    update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,35 +9,33 @@ const App: FC = () => {
       defaultBackground="ghostwhite"
       data={[
         {
+          id: 0,
+          pictures: 'https://fortegrp.com/wp-content/uploads/2024/02/LinkedInBanner-Option-2.png',
           title: "Title 1",
           content: `It began with an idea: help developers deliver better quality software.But evolution is inherent to business—and technology—and we’ve evolved, too. Today, we connect the right people to the right technology, devise optimized processes that deliver exceptional quality software, and create a path to your vision’s realization.
           Everyone has their own story to tell, their own journey to take, and their own path to get there. At Forte Group, we make the path easier. We remove the common obstacles from getting from Point A to Point B. A smoother road. An engine to accelerate. A bridge between people and technology. A pathway to what’s possible.`,
-          // pictures:
-          //   "https://fortegrp.com/wp-content/uploads/2024/02/PreviewBanner.jpg",
+   
         },
 
-        {
+        { id: 1,
+          pictures: 'https://fortegrp.com/wp-content/uploads/2024/02/HeroBanner-Option-2.png',
           title: "Title 2",
           content: "Hello guys2",
-          // pictures:
-          //   "https://fortegrp.com/wp-content/uploads/2024/02/LinkedInBanner-Option-2.png",
+       
         },
 
-        {
+        { id: 2,
           title: "Title 3",
           content: "Hello guys3",
-          // pictures:
-          //   "https://fortegrp.com/wp-content/uploads/2024/02/ai-integration-for-devops-and-platform-engineering.png",
+         
         },
-
-        {
+        { id: 3,
           title: "Title 4",
           content: "Hello guys4",
-          // pictures:
-          //   "https://fortegrp.com/wp-content/uploads/2024/01/HeroBanner-Option-1-1.png",
+
         },
       ]}
-      bgColors={["skyblue", "violet", "pink", "cyan"]}
+      // bgColors={["skyblue", "violet", "pink", "cyan"]}
     />
   );
 };
--- src/components/KeyNotes.tsx
+++ src/components/KeyNotes.tsx
@@ -1,26 +1,17 @@
-import {
-  FC,
-  ReactNode,
-  memo,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
-
-/*
-
- Docs:
+import { FC, ReactNode, memo, useEffect, useMemo, useRef, useState } from "react";
 
 
-
-*/
 
 type KeyNotesProps = {
-  data?: Array<{ title?: string; content?: string; pictures?: string }>;
+  data?: Array<{
+    title?: string;
+    content?: string;
+    pictures?: string;
+    id: number;
+  }>;
   textColor?: string;
   enableControls?: boolean;
-  autoSlide?: {on?: boolean, speed?: number};
+  autoSlide?: { on?: boolean; speed?: number };
   position?: boolean;
   fontSize?: string;
   render?: (data?: {
@@ -36,7 +27,9 @@ type KeyNotesProps = {
 type KeyNotesState = {
   order: number;
   index: number;
+  controlsOut: boolean;
   position: boolean;
+  autoSlide?: { on?: boolean; speed?: number };
   isPicFull: boolean;
 };
 
@@ -45,7 +38,7 @@ const KeyNotes: FC<KeyNotesProps> = memo(
     data,
     bgColors,
     defaultBackground,
-    autoSlide = {on: false},
+    autoSlide = { on: false },
     position = false,
     render,
     enableControls = true,
@@ -55,11 +48,14 @@ const KeyNotes: FC<KeyNotesProps> = memo(
     const [state, setState] = useState<KeyNotesState>({
       order: 1,
       index: 0,
+      controlsOut: true,
       position: !position,
+      autoSlide: {on: autoSlide.on, speed: autoSlide.speed},
       isPicFull: false,
     });
 
     const KeyNoteRef = useRef<HTMLDivElement>(null);
+    let timer = useRef<any>(null)
 
     const keyNoteWrapperStyle: React.CSSProperties = useMemo(
       () => ({
@@ -68,14 +64,18 @@ const KeyNotes: FC<KeyNotesProps> = memo(
         justifyContent: "center",
         alignItems: "center",
         color: textColor,
-        transition: "0.5s all ease-in-out",
+        transition: "0.5s all ease",
         fontSize,
         backgroundColor: bgColors ? bgColors[state.index] : defaultBackground,
-        height: "100vh",
+        minHeight: "100vh",
         width: "100vw",
       }),
       [state.index, state.order]
     );
+
+    const sidePreviewStyle: React.CSSProperties = {
+      display: "flex",
+    };
 
     const picStyle: React.CSSProperties = useMemo(
       () => ({
@@ -93,7 +93,10 @@ const KeyNotes: FC<KeyNotesProps> = memo(
     body,html {
         margin: 0;
         padding: 0;
-        background-color: ${bgColors ? bgColors[state.index] : defaultBackground};
+        font-size: clamp(16px, 2vw, 18px);
+        background-color: ${
+          bgColors ? bgColors[state.index] : defaultBackground
+        };
         overflow-x: hidden;
         overflow-y: auto;
         height: 100%;
@@ -101,6 +104,34 @@ const KeyNotes: FC<KeyNotesProps> = memo(
         font-family: 'Arial';
     }
 
+    body::after {
+      content: '';
+      position: fixed;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      z-index: -1;
+      pointer-events: none;
+      overflow-y: scroll;
+      scrollbar-width: thin;
+      scrollbar-color: #888888 #f5f5f5;
+    }
+    
+    ::-webkit-scrollbar {
+      width: 10px;
+      background: #e4e4e4
+    }
+    
+  
+    ::-webkit-scrollbar-thumb {
+      background-color: #787878;
+      border-radius: 5px;
+    }
+    
+    ::-webkit-scrollbar-thumb:hover {
+      background-color: #555555;
+    }
     
     `;
 
@@ -125,10 +156,17 @@ const KeyNotes: FC<KeyNotesProps> = memo(
       };
     };
 
+    const setControllsOut = (isOut: boolean) => {
+      setState({
+        ...state,
+        controlsOut: isOut
+      })
+    }
+
     const slideByArrows = (e: KeyboardEvent) => {
-      if (e.key === "ArrowRight") {
+      if (e.key === "ArrowRight" || e.key === "ArrowDown") {
         forward(1)();
-      } else if (e.key === "ArrowLeft") {
+      } else if (e.key === "ArrowLeft" || e.key === "ArrowUp") {
         if (state.index === 0) {
           return;
         }
@@ -153,6 +191,23 @@ const KeyNotes: FC<KeyNotesProps> = memo(
       };
     };
 
+    const autoForward = () => {
+      timer!.current = setInterval(() => {
+          forward(1)();
+        },
+        state.autoSlide?.speed ? state.autoSlide.speed * 1000 : 1000
+      );
+    }
+
+    const autoPlay = () => {
+      setState((prevState) => (
+        {
+          ...prevState,
+          autoSlide: {...prevState.autoSlide, on: true}
+        }
+      ))
+    }
+
     const setFullScreen = () => {
       if (KeyNoteRef.current) {
         KeyNoteRef.current.requestFullscreen();
@@ -167,10 +222,7 @@ const KeyNotes: FC<KeyNotesProps> = memo(
     };
 
     useEffect(() => {
-      if (
-        state.index >= (data?.length || 0) ||
-        state.index >= (bgColors?.length || 0)
-      ) {
+      if (state.index >= (data?.length || 0)) {
         setState({
           ...state,
           order: 1,
@@ -182,18 +234,16 @@ const KeyNotes: FC<KeyNotesProps> = memo(
     }, [state.index, data, bgColors]);
 
     useEffect(() => {
-        let timer: any = null
+ 
+      if (state.autoSlide?.on) {
 
-        if(autoSlide.on){
-            timer = setInterval(() => {
-                forward(1)()
-            }, autoSlide.speed ? autoSlide.speed * 1000 : 1000)
-        }
+        autoForward()
+      }
 
-        return () => {
-            clearInterval(timer)
-        }
-    }, [autoSlide.on])
+      return () => {
+        clearInterval(timer.current);
+      };
+    }, [state.autoSlide?.on]);
 
     useEffect(() => {
       document.documentElement.addEventListener("keydown", slideByArrows);
@@ -205,104 +255,184 @@ const KeyNotes: FC<KeyNotesProps> = memo(
 
     return (
       <>
-        <div ref={KeyNoteRef} style={keyNoteWrapperStyle} id="keynote-wrapper">
-          {render ? (
-            render({
-              content: data && data[state.index]?.content,
-              title: data && data[state.index]?.title,
-              order: state.order,
-              pics: data && data[state.index]?.pictures,
-            })
-          ) : (
-            <div
-              style={{
-                display: "flex",
-                padding: "2rem",
-                justifyContent: data && !data[state.index]?.pictures ? 'center': 'flex-start',
-                alignItems: state.position ? 'initial': 'center',
-                aspectRatio: "16/9",
-                flexDirection: state.position ? "column" : "row",
-                gap: 30,
-              }}
-              id="keynote-content"
-            >
-              <div style={{ width: !state.position ? "33.333%" : "100%" }} id="content">
-                <h2>{data ? data[state.index]?.title : "Default title"}</h2>
-                <p>{data ? data[state.index]?.content : "Default text"}</p>
-              </div>
+        <div style={sidePreviewStyle} className="keynote-main">
+          <div
+            style={{
+              width: "15%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              borderRight: "1px solid silver",
+              minHeight: "100vh",
+              overflow: "auto",
+            }}
+            className="side-preview"
+          >
+            {data
+              ? data.map((item, i: number) => (
+                  <div
+                    
+                    onMouseEnter={() => {
+                      clearTimeout(timer.current)
+                      setState((prevState) => (
+                        {
+                          ...prevState,
+                          index: i,
+                          order: i + 1,
+                          autoSlide: {...prevState.autoSlide, on: false}
+                        }
+                      ))
+                    }}
+                 
+                    style={{
+                      border:
+                        state.index === i
+                          ? "5px solid #4989dcf2"
+                          : "1px solid #ccc",
+                      borderBottom:
+                        state.index === i ? "5px solid #4989dcf2" : 0,
+                      borderLeft: state.index === i ? "5px solid #4989dcf2" : 0,
+                      padding: 10,
+                      backgroundImage: data && data[i]?.pictures ? `url(${data[i]?.pictures})` : '',
+                      backgroundRepeat: 'no-repeat',
+                      backgroundSize: 'cover',
+                      height: "100%",
+                      cursor: "pointer",
+                      backgroundColor: bgColors ? bgColors[i] : "#eff6ff",
+                      borderRight:
+                        state.index === i ? "5px solid #4989dcf2" : 0,
+                    }}
+                    key={crypto.randomUUID()}
+                  >
+                    {item.title}
+                  </div>
+                ))
+              : null}
+          </div>
 
-              {data && data[state.index]?.pictures ? <div id="pic">
-                <img
-                  style={state.isPicFull ? picStyle : { maxWidth: "100%" }}
-                  src={data ? data[state.index]?.pictures : ""}
-                  alt=""
-                />
-              </div>: null}
-            </div>
-          )}
-
-          {enableControls ? (
-            <div
-              style={{
-                position: "absolute",
-                right: 20,
-                top: 0,
-                // bottom: 0,
-                zIndex: 3,
-                display: "flex",
-                flexWrap: "wrap",
-                gap: 20,
-                padding: 10,
-              }}
-              id="keynote-controls"
-            >
-              <button
-                style={controlsStyles}
-                disabled={state.index === 0}
-                onClick={back(1)}
+          <div
+            onMouseEnter={() => setControllsOut(false)}
+            onMouseLeave={() => setControllsOut(true)}
+            ref={KeyNoteRef}
+            style={keyNoteWrapperStyle}
+            id="keynote-wrapper"
+          >
+            {render ? (
+              render({
+                content: data && data[state.index]?.content,
+                title: data && data[state.index]?.title,
+                order: state.order,
+                pics: data && data[state.index]?.pictures,
+              })
+            ) : (
+              <div
+                style={{
+                  display: "flex",
+                  padding: "2rem",
+                  justifyContent:
+                    data && !data[state.index]?.pictures
+                      ? "center"
+                      : "flex-start",
+                  alignItems: state.position ? "initial" : "center",
+                  aspectRatio: "16/9",
+                  flexDirection: state.position ? "column" : "row",
+                  gap: 30,
+                }}
+                id="keynote-content"
               >
-                Back
-              </button>
-              <button style={controlsStyles} onClick={forward(1)}>
-                Forward
-              </button>
-              <button style={controlsStyles} onClick={() => setFullScreen()}>
-                Full screen
-              </button>
+                <div
+                  style={{ width: !state.position ? "50%" : "100%" }}
+                  id="content"
+                >
+                  {" "}
+                  {data && data[state.index]?.pictures ? (
+                    <div id="pic">
+                      <img
+                        style={
+                          state.isPicFull ? picStyle : { maxWidth: "100%", width: 700, transition: "0.3s all ease",}
+                        }
+                        src={data ? data[state.index]?.pictures : ""}
+                        alt=""
+                      />
+                    </div>
+                  ) : null}
+                  <h2>{data ? data[state.index]?.title : "Default title"}</h2>
+                  <p>{data ? data[state.index]?.content : "Default text"}</p>
+                </div>
+              </div>
+            )}
 
-              <button style={controlsStyles} onClick={changePosition}>
-                Change orientation
-              </button>
+            {enableControls ? (
+              <div
+                style={{
+                  position: "absolute",
+                  transition: '0.3s all ease',
+                  right: 20,
+                  top: state.controlsOut ? '-700px': 0,
+                  zIndex: 3,
+                  display: "flex",
+                  flexWrap: "wrap",
+                  gap: 20,
+                  padding: 10,
+                }}
+                id="keynote-controls"
+              >
+                <button
+                  style={controlsStyles}
+                  disabled={state.index === 0}
+                  onClick={back(1)}
+                >
+                  Back
+                </button>
+                <button style={controlsStyles} onClick={forward(1)}>
+                  Forward
+                </button>
+                <button style={controlsStyles} onClick={autoPlay}>
+                  Auto Play
+                </button>
+                <button style={controlsStyles} onClick={() => setFullScreen()}>
+                  Full screen
+                </button>
 
-              <button style={controlsStyles} onClick={setPicFullScreen}>
-                Fullscreen pic
-              </button>
-              {state.order}
-            </div>
-          ) : (
-            <div
-              style={{
-                position: "absolute",
-                right: 20,
-                top: 0,
-                zIndex: 3,
-                display: "flex",
-                flexWrap: "wrap",
-                gap: 20,
-                padding: 10,
-              }}
-              id="keynote-controls"
-            >
-              <button style={controlsStyles} onClick={changePosition}>
-                Change orientation
-              </button>
+                <button style={controlsStyles} onClick={changePosition}>
+                  Change orientation
+                </button>
 
-              <button style={controlsStyles} onClick={setPicFullScreen}>
-                Fullscreen pic
-              </button>
-              {state.order}
-            </div>
-          )}
+                <button
+                  disabled={data && !data[state.index]?.pictures}
+                  style={controlsStyles}
+                  onClick={setPicFullScreen}
+                >
+                  Fullscreen pic
+                </button>
+                {state.order}
+              </div>
+            ) : (
+              <div
+                style={{
+                  position: "absolute",
+                  right: 20,
+                  top: state.controlsOut ? '-700px': 0,
+                  transition: '0.3s all ease',
+                  zIndex: 3,
+                  display: "flex",
+                  flexWrap: "wrap",
+                  gap: 20,
+                  padding: 10,
+                }}
+                id="keynote-controls"
+              >
+                <button style={controlsStyles} onClick={changePosition}>
+                  Change orientation
+                </button>
+
+                <button style={controlsStyles} onClick={setPicFullScreen}>
+                  Fullscreen pic
+                </button>
+                {state.order}
+              </div>
+            )}
+          </div>
         </div>
 
         <style>{mainStyle}</style>
